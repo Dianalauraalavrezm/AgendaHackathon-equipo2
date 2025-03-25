941441cd5a80f135977db583cd868fb6935eef8b
Denisse Hernández <denisse.hernandezvazquez16@gmail.com>
2025-03-25 15:36:41 -0600

    Agregue añadir contactos y contacto ya existente

--- AgendaHackathon2/src/main/java/org/generation/model/Agenda.java
+++ AgendaHackathon2/src/main/java/org/generation/model/Agenda.java
@@ -1,64 +1,14 @@
-ppackage org.generation.metodos;
+package org.generation.model;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-import org.generation.model.Contactos;
 
-public class AgendaService {
-    private Scanner scanner;
+// Almacena información de contactos
+public class Agenda {
+
     private ArrayList<Contactos> contactos;
     private static final int LimiteContacto = 10;
 
-    /** Constructor **/
-    public AgendaService() {
-        this.scanner = new Scanner(System.in);
-        this.contactos = new ArrayList<>();
+    public Agenda(ArrayList<Contactos> contactos) {
+        this.contactos = contactos;
     }
-
-    /** Registrar Contacto **/
-    public void registrarContacto() {
-        // Limite de contactos
-        if (contactos.size() >= LimiteContacto) {
-            System.out.println("Almacenamiento lleno. No se pueden agregar más contactos.");
-            return;
-        }
-
-        /** Solicitar los Datos **/
-        // Solicitar datos al usuario
-        System.out.print("Ingrese el nombre: ");
-        String nombre = scanner.nextLine();
-
-        System.out.print("Ingrese el apellido: ");
-        String apellido = scanner.nextLine();
-
-        System.out.print("Ingrese el teléfono: ");
-        String telefono = scanner.nextLine();
-
-        // Verificar si el contacto existe
-        if (existeContacto(nombre, apellido)) {
-            System.out.println("Este contacto ya está registrado");
-            return;
-        }
-
-        // Crear nuevo contacto y agregarlo a la lista
-        int nuevoIndex = contactos.size() + 1;
-        Contactos nuevoContacto = new Contactos(nuevoIndex, nombre, apellido, telefono);
-        contactos.add(nuevoContacto);
-
-        System.out.println("Contacto agregado con éxito: " + nuevoContacto);
-    }
-
-    /** Verificar si el contacto ya existe **/
-    public boolean existeContacto(String nombre, String apellido) {
-        for (Contactos contacto : contactos) {
-            if (contacto.getNombre().equalsIgnoreCase(nombre) &&
-                    contacto.getApellido().equalsIgnoreCase(apellido)) {
-                return true;  // Ya existe el contacto
-            }
-        }
-        return false;  // El contacto no existe
-    }
-
-
-
-}
+}// class Agenda
