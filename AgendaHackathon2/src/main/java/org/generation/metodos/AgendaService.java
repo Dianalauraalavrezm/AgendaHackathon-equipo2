353799862d01a98889e9985257e50665b1c01119
itzelpadillac <padillaitzel090@gmail.com>
2025-03-25 15:43:52 -0600

    primer actualización agendaLlena y espacioslibres

--- AgendaHackathon2/src/main/java/org/generation/metodos/AgendaService.java
+++ AgendaHackathon2/src/main/java/org/generation/metodos/AgendaService.java
@@ -1,6 +1,22 @@
 package org.generation.metodos;
 
+import org.generation.excepciones.AgendaLlena;
+import org.generation.excepciones.AgendaLlenaException;
+import org.generation.model.Contactos;
+
 public class AgendaService {
+    /** Indica si la agenda esta llena y muestra una Excepcion */
+    public boolean agendaLlena() throws AgendaLlenaException {
+        for (int i = 0; i < contactos.length; i++){
+            if (contactos[i] == null);{
 
 
+            }
+        }
+
+    public void espaciosLibres() {
+        int espaciosLibres = tamanoMaximo - contactos.size();
+        System.out.println("Quedan " + espaciosLibres + " espacios libres en la agenda.");
+    }
+    }
 }
